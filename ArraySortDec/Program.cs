d59960124258e13639750204d98307218774f293
nafirk99 <nafiz.ba.pakizatvl@gmail.com>
2024-02-05 14:27:34 +0600

    arraysort desc

---
+++ ArraySortDec/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Enter Number Of Elements: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] arr = new int[n];
+Console.Write("Enter Elements: ");
+for (int i = 0; i < n; i++)
+{
+    arr[i] = int.Parse(Console.ReadLine());
+}
+int temp;
+for (int i = 0; i < arr.Length - 1; i++)
+{
+	for (int j = i+1; j < arr.Length; j++)
+	{
+		if (arr[i] < arr[j])
+		{
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
+foreach (var item in arr)
+{
+    Console.WriteLine(item);
+}
---
+++ ArraySortLINQ/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter Number Of Elements: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] arr = new int[n];
+Console.Write("Enter Elements: ");
+for (int i = 0; i < n; i++)
+{
+    arr[i] = int.Parse(Console.ReadLine());
+}
